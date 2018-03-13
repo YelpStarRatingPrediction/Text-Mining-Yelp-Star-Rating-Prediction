65129914ae589b5e82f822e1a27ec2289cd1832a
GuanxuSu <guanxu.su@gmail.com>
2018-03-13 16:30:00 -0500

    update

--- code/step7_kaggle prediction.py
+++ code/step7_kaggle prediction.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 
 # read the data from disk and split into lines
-# we use .strip() to remove the final (empty) line
 
 train = pd.read_csv("data/train_sample.csv")
 test = pd.read_csv("data/test_clean.csv")
@@ -18,11 +17,11 @@ test = pd.read_csv("data/test_clean.csv")
 
 # we're interested in the text of each review and the stars rating
 # so we load these into separate lists
+"""Below are 2-step models"""
 texts_train = train["text"]
 stars_train = train["stars"]
 train_id = train["commendid"]
 texts_test = test["text"]
-#stars_test = test["stars"]
 test_id = test["Id"]
 
 whole_text = pd.concat([texts_train,texts_test])
@@ -110,29 +109,95 @@ X_test_negative = vectors_test_negative
 y_train_negative = y_train_negative.astype(int)
 
 # initialise the SVM classifier
-# classifier = LinearSVC(C=0.2)       
+classifier = LinearSVC(C=0.2)       
 from sklearn.linear_model import LinearRegression
 # train the classifier
 
-# classifier.fit(X_train_positive, y_train_positive)
+classifier.fit(X_train_positive, y_train_positive)
 regr = LinearRegression().fit(X_train_positive, y_train_positive)
-positive_preds = regr.predict(X_test_positive)
+positive_preds1 = regr.predict(X_test_positive)
+positive_preds2 = regr.predict(X_test_positive)
 test_id_positive = np.array(test_id_positive, dtype=int)
-positive_predictions = pd.concat([pd.DataFrame(test_id_positive,columns=['commendid']),pd.DataFrame(positive_preds,columns=['predict_stars'])],axis=1)
+positive_predictions1 = pd.concat([pd.DataFrame(test_id_positive,columns=['commendid']),pd.DataFrame(positive_preds1,columns=['predict_stars'])],axis=1)
+positive_predictions2 = pd.concat([pd.DataFrame(test_id_positive,columns=['commendid']),pd.DataFrame(positive_preds2,columns=['predict_stars'])],axis=1)
 
-# classifier.fit(X_train_negative, y_train_negative)
+classifier.fit(X_train_negative, y_train_negative)
 regr = LinearRegression().fit(X_train_negative, y_train_negative)
-negative_preds = regr.predict(X_test_negative)
+negative_preds1 = regr.predict(X_test_negative)
+negative_preds2 = regr.predict(X_test_negative)
 test_id_negative = np.array(test_id_negative, dtype=int)
-negative_predictions = pd.concat([pd.DataFrame(test_id_negative,columns=['commendid']),pd.DataFrame(negative_preds,columns=['predict_stars'])],axis=1)
+negative_predictions1 = pd.concat([pd.DataFrame(test_id_negative,columns=['commendid']),pd.DataFrame(negative_preds1,columns=['predict_stars'])],axis=1)
+negative_predictions2 = pd.concat([pd.DataFrame(test_id_negative,columns=['commendid']),pd.DataFrame(negative_preds2,columns=['predict_stars'])],axis=1)
 
-predictions = pd.concat([positive_predictions,negative_predictions],axis=0)
-predictions['predict_stars'] = [5 if (y>=4.8) else y for y in predictions['predict_stars']]
-predictions['predict_stars'] = [1 if (y<=1.1) else y for y in predictions['predict_stars']]
+predictions1 = pd.concat([positive_predictions1,negative_predictions1],axis=0)
+predictions1['predict_stars'] = [5 if (y>=4.8) else y for y in predictions1['predict_stars']]
+predictions1['predict_stars'] = [1 if (y<=1.1) else y for y in predictions1['predict_stars']]
+predictions2 = pd.concat([positive_predictions2,negative_predictions2],axis=0)
+predictions2['predict_stars'] = [5 if (y>=4.8) else y for y in predictions2['predict_stars']]
+predictions2['predict_stars'] = [1 if (y<=1.1) else y for y in predictions2['predict_stars']]
 
 
-whole_predict = test2.set_index('commendid').join(predictions.set_index('commendid'))
+whole_predict1 = test2.set_index('commendid').join(predictions1.set_index('commendid'))
+whole_predict2 = test2.set_index('commendid').join(predictions2.set_index('commendid'))
+
 
 #write as csv file
-whole_predict.to_csv("result.csv",sep=',')
+whole_predict1.to_csv("result1.csv",sep=',')   #SVC+SVC result
+whole_predict2.to_csv("result2.csv",sep=',')   #SVC+Linear result
 
+########################################################################################
+
+"""Below are 1-step models"""
+texts_train = train["text"]
+stars_train = train["stars"]
+train_id = train["commendid"]
+texts_test = test["text"]
+test_id = test["Id"]
+
+whole_text = pd.concat([texts_train,texts_test])
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+# This vectorizer breaks text into single words and bi-grams
+# and then calculates the TF-IDF representation
+vectorizer = TfidfVectorizer(ngram_range=(1,2))
+
+
+# the 'fit' builds up the vocabulary from all the reviews
+# while the 'transform' step turns each indivdual text into
+# a matrix of numbers.
+#vectors_train = vectorizer.fit_transform(texts_train.values.astype('U'))
+#vectors_test = vectorizer.fit_transform(texts_test.values.astype('U'))
+vectors = vectorizer.fit_transform(whole_text.values.astype('U'))
+vectors_train = vectors[0:400000]
+vectors_test = vectors[400000:]
+vectors_kaggle = vectors[400000:]
+
+X_train = vectors_train
+X_test = vectors_test
+y_train = stars_train
+y_test = stars_test
+
+"""Support Vector Classifier"""
+
+# initialise the SVM classifier
+classifier = LinearSVC(C=0.2)  
+# train the classifier
+
+classifier.fit(X_train, y_train)
+
+preds = classifier.predict(X_kaggle)  #prediction
+whole_predict3 = pd.concat([pd.DataFrame(test_id,columns=['Id']),pd.DataFrame(preds,columns=['Prediction1'])],axis=1)
+whole_predict3.to_csv("result3.csv",sep=',')   #SVC result
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, preds))
+
+
+"""Naive Bayes Method"""
+from sklearn.naive_bayes import MultinomialNB
+nb = MultinomialNB()
+nb.fit(X_train, y_train)
+
+preds_nb = nb.predict(X_test)
+whole_predict4 = pd.concat([pd.DataFrame(test_id,columns=['Id']),pd.DataFrame(preds_nb,columns=['Prediction1'])],axis=1)
+whole_predict4.to_csv("result4.csv",sep=',')   #Naive Bayes result
